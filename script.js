6ba767a3994ad380cd56788a18100958a5a4f04a
Chris <93837672+LazySlasherTrain@users.noreply.github.com>
2022-02-11 22:05:28 +0100

    First commit to this small training app

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+document.querySelector('.ar1').addEventListener('click', function () {
+  document
+    .querySelector('.list__done')
+    .appendChild(document.querySelector('.pierwszy'));
+});
