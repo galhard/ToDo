1796bc037a0b6a65a55ca33ae7fba4a24ff8b669
galhard <93837672+galhard@users.noreply.github.com>
2022-02-12 22:29:02 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 'use strict';
 
-document.querySelector('.ar1').addEventListener('click', function () {
-  document
-    .querySelector('.list__done')
-    .appendChild(document.querySelector('.pierwszy'));
-});
+//Moving from to-do to done
+const arrowRight = document.getElementsByName('arrow-forward-circle-outline');
+for (const arrow of arrowRight) {
+  arrow.addEventListener('click', function () {
+    document
+      .querySelector('.list__done')
+      .appendChild(arrow.closest('.list__tasks__task'));
+    arrow.parentNode.removeChild(arrow.parentNode.firstElementChild);
+    arrow.parentNode.removeChild(arrow.parentNode.lastElementChild);
+  });
+}
+//Using prompt to change text
+const addCircle = document.getElementsByName('add-circle-outline');
+for (const add of addCircle) {
+  add.addEventListener('click', function () {
+    let text = prompt('What is your task');
+    add.parentNode.querySelector('.task__name').textContent = text;
+  });
+}
