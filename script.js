c8a11b512f09814cd4d605820ce281b99dac07ed
galhard <93837672+galhard@users.noreply.github.com>
2022-02-14 20:02:39 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -19,3 +19,22 @@ for (const add of addCircle) {
     add.parentNode.querySelector('.task__name').textContent = text;
   });
 }
+
+const inputText = document.querySelector('.input-text');
+
+// inputText.addEventListener('keypress', function (e) {
+//   if (e.key === 'Enter') {
+//     document.querySelector('.testing').textContent = inputText.value;
+//     inputText.value = '';
+//   }
+// });
+const taskName = document.querySelectorAll('.task__name');
+
+inputText.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    for (const task of taskName) {
+      task.textContent = inputText.value;
+      inputText.value = '';
+    }
+  }
+});
