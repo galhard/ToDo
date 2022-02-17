d3db12b2cb0b830620fb5cf3fd9343084a01d9c7
galhard <93837672+galhard@users.noreply.github.com>
2022-02-17 23:55:40 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,73 @@
 'use strict';
 
-//Moving from to-do to done
-const arrowRight = document.getElementsByName('arrow-forward-circle-outline');
-for (const arrow of arrowRight) {
-  arrow.addEventListener('click', function () {
-    document
-      .querySelector('.list__done')
-      .appendChild(arrow.closest('.list__tasks__task'));
-    arrow.parentNode.removeChild(arrow.parentNode.firstElementChild);
-    arrow.parentNode.removeChild(arrow.parentNode.lastElementChild);
-  });
-}
-//Using prompt to change text
-const addCircle = document.getElementsByName('add-circle-outline');
-for (const add of addCircle) {
-  add.addEventListener('click', function () {
-    let text = prompt('What is your task');
-    add.parentNode.querySelector('.task__name').textContent = text;
-  });
-}
-
 const inputText = document.querySelector('.input-text');
+const listToDo = document.querySelector('.list__todo');
+const cong = document.querySelector('.cong');
+function working() {
+  for (const r of document.querySelectorAll('.list__todo__task')) {
+    let arrow = r.querySelector('.arrow-right');
+    let del = r.querySelector('.delete-task');
+    r.addEventListener('mouseenter', function () {
+      arrow.classList.remove('hide');
+      del.classList.remove('hide');
+      r.classList.add('border');
 
-// inputText.addEventListener('keypress', function (e) {
-//   if (e.key === 'Enter') {
-//     document.querySelector('.testing').textContent = inputText.value;
-//     inputText.value = '';
-//   }
-// });
-const taskName = document.querySelectorAll('.task__name');
+      r.addEventListener('mouseleave', function () {
+        arrow.classList.add('hide');
+        del.classList.add('hide');
+        r.classList.remove('border');
+      });
+      del.addEventListener('click', function () {
+        r.remove();
+      });
+      arrow.addEventListener('click', function () {
+        r.removeChild(arrow);
+        r.removeChild(del);
+        document.querySelector('.list__done').appendChild(r);
+        r.className = 'list__done__task';
+        if (!listToDo.hasChildNodes()) {
+          cong.classList.remove('hide');
+          document
+            .querySelector('.cong__text-close')
+            .addEventListener('click', function () {
+              cong.classList.add('hide');
+            });
+        }
+      });
+    });
+  }
+}
 
+//After press ENTER
 inputText.addEventListener('keypress', function (e) {
   if (e.key === 'Enter') {
-    for (const task of taskName) {
-      task.textContent = inputText.value;
-      inputText.value = '';
-    }
+    listToDo.innerHTML += `<div class="list__todo__task"><ion-icon class="delete-task hide" name="trash-outline"></ion-icon></ion-icon>
+    <div class="task__name">${inputText.value}</div>
+    <ion-icon class="arrow-right hide" name="arrow-forward-outline"></ion-icon>
+  </div>`;
+    inputText.value = '';
   }
+  working();
 });
+//After click PLUS
+const inputBox = document.querySelector('.input');
+const addTask = document.querySelector('.add__task');
+addTask.addEventListener('click', function () {
+  if (inputText.value != '') {
+    listToDo.innerHTML += `<div class="list__todo__task"><ion-icon class="delete-task hide" name="trash-outline"></ion-icon></ion-icon>
+    <div class="task__name">${inputText.value}</div>
+    <ion-icon class="arrow-right hide" name="arrow-forward-outline"></ion-icon>
+  </div>`;
+    inputText.value = '';
+  } else {
+    alert('type some task in the text field'.toUpperCase());
+  }
+  working();
+});
+inputBox.addEventListener('mouseenter', function () {
+  addTask.classList.remove('hide');
+});
+inputBox.addEventListener('mouseleave', function () {
+  addTask.classList.add('hide');
+});
+//ALL TASK DONE INFO
