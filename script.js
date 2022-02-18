210c113a7a324215ea4eff2bf8154abf48806147
galhard <93837672+galhard@users.noreply.github.com>
2022-02-18 20:20:52 +0100

    fix#2

--- script.js
+++ script.js
@@ -3,6 +3,17 @@
 const inputText = document.querySelector('.input-text');
 const listToDo = document.querySelector('.list__todo');
 const cong = document.querySelector('.cong');
+
+function congrat() {
+  if (!listToDo.hasChildNodes()) {
+    cong.classList.remove('hide');
+    document
+      .querySelector('.cong__text-close')
+      .addEventListener('click', function () {
+        cong.classList.add('hide');
+      });
+  }
+}
 function working() {
   for (const r of document.querySelectorAll('.list__todo__task')) {
     let arrow = r.querySelector('.arrow-right');
@@ -19,36 +30,33 @@ function working() {
       });
       del.addEventListener('click', function () {
         r.remove();
+        congrat();
       });
       arrow.addEventListener('click', function () {
         r.removeChild(arrow);
         r.removeChild(del);
         document.querySelector('.list__done').appendChild(r);
         r.className = 'list__done__task';
-        if (!listToDo.hasChildNodes()) {
-          cong.classList.remove('hide');
-          document
-            .querySelector('.cong__text-close')
-            .addEventListener('click', function () {
-              cong.classList.add('hide');
-            });
-        }
+        congrat();
       });
     });
   }
 }
-
+//##ADDING DIV##
+function addingTask() {
+  listToDo.innerHTML += `<div class="list__todo__task"><ion-icon class="delete-task hide" name="trash-outline"></ion-icon></ion-icon>
+  <div class="task__name">${inputText.value}</div>
+  <ion-icon class="arrow-right hide" name="arrow-forward-outline"></ion-icon>
+</div>`;
+  inputText.value = '';
+}
 //After press ENTER
 inputText.addEventListener('keypress', function (e) {
   if (e.key === 'Enter') {
     if (inputText.value === '') {
       alert('type some task in the text field'.toUpperCase());
     } else {
-      listToDo.innerHTML += `<div class="list__todo__task"><ion-icon class="delete-task hide" name="trash-outline"></ion-icon></ion-icon>
-    <div class="task__name">${inputText.value}</div>
-    <ion-icon class="arrow-right hide" name="arrow-forward-outline"></ion-icon>
-  </div>`;
-      inputText.value = '';
+      addingTask();
     }
   }
   working();
@@ -58,19 +66,20 @@ const inputBox = document.querySelector('.input');
 const addTask = document.querySelector('.add__task');
 addTask.addEventListener('click', function () {
   if (inputText.value != '') {
-    listToDo.innerHTML += `<div class="list__todo__task"><ion-icon class="delete-task hide" name="trash-outline"></ion-icon></ion-icon>
-    <div class="task__name">${inputText.value}</div>
-    <ion-icon class="arrow-right hide" name="arrow-forward-outline"></ion-icon>
-  </div>`;
-    inputText.value = '';
+    addingTask();
+    inputText.focus();
   } else {
     alert('type some task in the text field'.toUpperCase());
   }
   working();
 });
-inputBox.addEventListener('mouseenter', function () {
+
+//Add/Remove plus sign in input field
+inputBox.addEventListener('focusin', function () {
+  inputBox.style.width = '200%';
   addTask.classList.remove('hide');
 });
-inputBox.addEventListener('mouseleave', function () {
+inputBox.addEventListener('focusout', function () {
+  inputBox.style.width = '100%';
   addTask.classList.add('hide');
 });
